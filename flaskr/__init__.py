fd3582263758009d39d6cd41e87925688ef577f3
MiFy <144579579+kostyakuz@users.noreply.github.com>
2024-03-24 10:22:03 +0600

    final

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -3,13 +3,18 @@ from flask import render_template
 
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
+    app.config['TEMPLATES_AUTO_RELOAD'] = True
 
     @app.route("/")
     def display_main_page():
         return render_template("index.html")
     
-    @app.route("/resources")
-    def display_resources_list():
-        return render_template("resources_list.html")
+    @app.route("/sport_cars")
+    def display_sport_cars_list():
+        return render_template("sport_cars_list.html")
+    
+    @app.route("/not_sport_cars")
+    def display_not_sport_cars_list():
+        return render_template("not_sport_cars_list.html")
 
     return app
